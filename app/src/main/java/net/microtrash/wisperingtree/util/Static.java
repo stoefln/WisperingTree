a6219c8f7de1a3b6073f8c1445d2a929ccf55f70
Stephan Petzl <stephan.petzl@gmail.com>
2015-05-31 11:10:51 -0400

    Change mac addresses

--- app/src/main/java/net/microtrash/wisperingtree/util/Static.java
+++ app/src/main/java/net/microtrash/wisperingtree/util/Static.java
@@ -8,22 +8,26 @@ import java.util.Hashtable;
 public class Static {
     public static final String FOLDER_NAME = "WisperingTree";
     public static final String KEY_LAST_REC_NUM = "last rec number";
-    public static final String SERVER_MAC = "E4:12:1D:B4:DF:C5";
+
     public static final int MAX_FILES = 20;
     public static final String KEY_MIN_NOISE_VALUE = "min_noise_value";
     public static final String KEY_MAX_NOISE_VALUE = "max_noise_value";
+    public static final String BACKUP_DIR_NAME = "backup";
     private static Hashtable<String, String> mClients;
+
+    public static final String SERVER_MAC = "B4:CE:F6:77:27:B0";
 
     public static Hashtable<String, String> getClients() {
         if (mClients == null) {
             mClients = new Hashtable<>();
             mClients.put("40:B0:FA:F4:EC:B9", "LG-E430");
-            //mClients.put("HTC-ONE", "98:0D:2E:C0:30:86");
-            //mClients.put("D8:90:E8:FB:D8:C2", "S4");
-            //mClients.put("94:D7:71:E3:E6:61", "S3");
-            mClients.put("00:73:E0:14:ED:87", "Young White");
-            mClients.put("B4:CE:F6:77:27:B0", "HTC OPCV1");
-            mClients.put("AC:36:13:D9:C8:1E", "Galaxy S3 Mini");
+            mClients.put("70:F9:27:D4:EC:1E", "S3");
+            mClients.put("78:F7:BE:5E:60:54", "S4");
+            mClients.put("44:80:EB:1C:20:7E", "Moto G");
+            //mClients.put("4C:3C:16:16:4A:6A", "Y Black");
+            //mClients.put("B4:CE:F6:77:27:B0", "HTC OPCV1");
+            mClients.put("AC:36:13:D9:C8:1E", "S3 Mini");
+            mClients.put("E4:12:1D:B4:DF:C5", "S5");
         }
         return mClients;
     }
